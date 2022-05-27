346d53d4e09bf7a71d72833d7c6ea26e189b5649
AtrinDev <atrin.abbasian7@gmail.com>
2022-05-27 15:32:14 +0430

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Grocery List
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](grocery-list.jpg)
+
+### Links
+
+- GitHub URL: [GitHub Repository]()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS3 custom properties
+- React js
+- Flexbox
+- CSS Grid
+- React Icons
+
+## Author
+
+- Website - [My Website](https://www.atrindev.ir)
+- Linkedin - [My Linkedin](https://www.linkedin.com/in/atrindev)
---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+// use local storage
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
+
+  // show all alerts
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showALert(true, "please enter value", "danger");
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editId) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditId(null);
+      setIsEditing(false);
+      showALert(true, "value changed", "success");
+    } else {
+      showALert(true, "item added to the list", "success");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  // show alert
+  const showALert = (show = false, msg = "", type = "") => {
+    setAlert({ show, msg, type });
+  };
+
+  // show clear list alert
+  const clearList = () => {
+    showALert(true, "empty list", "danger");
+    setList([]);
+  };
+
+  // show remove item alert
+  const removeItem = (id) => {
+    showALert(true, "item removed", "danger");
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  // show edit item alert
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditId(id);
+    setName(specificItem.title);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showALert} list={list} />}
+        <h3>grocery list</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="text" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} editItem={editItem} removeItem={removeItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default App;
